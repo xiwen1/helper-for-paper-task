57cb50f5ef56d23765521803f26d4ab9ad20eeda
xiwen_youmu <79042591+xiwen1@users.noreply.github.com>
2023-04-19 20:52:16 +0800

    Update turn-to-md.py

--- turn-to-md.py
+++ turn-to-md.py
@@ -1,10 +1,14 @@
-file_names = ['4-8', '4-9', '4-10', '4-11+', '4-19', '4-20']
+import os
+
+file_names = os.listdir()
 md = open('chapter4.md', 'w')
 for file in file_names:
-    full_name = file + '.cpp'
-    f = open(full_name, 'r', encoding='utf-8')
+    if file.split('.')[1] != 'cpp':
+        continue
+    task_name = file.split('.')[0]
+    f = open(file, 'r', encoding='utf-8')
     codes = f.read()
-    md.write('## ' + file + '\n')
+    md.write('## ' + task_name + '\n')
     md.write('```cpp\n' + codes + '\n```\n')
     f.close()
 md.close()
